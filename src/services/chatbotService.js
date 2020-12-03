23b44c9edd1623a8ae21a96e109e7cf8afde6210
IslemBenFraj <islem.benfraj43@gmail.com>
2020-12-03 22:52:14 +0100

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -92,7 +92,95 @@ let sendMessage = (sender_psid, response) => {
     }
   });
 };
+let sendCategories = (sender_psid) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      //send a generic template message
+      let response = {
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "generic",
+            elements: [
+              {
+                title: "Welcome1!",
+                image_url: "https://petersfancybrownhats.com/company_image.png",
+                subtitle: "We have the right hat for everyone.",
+                default_action: {
+                  type: "web_url",
+                  url: "https://petersfancybrownhats.com/view?item=103",
+                  webview_height_ratio: "tall",
+                },
+                buttons: [
+                  {
+                    type: "web_url",
+                    url: "https://petersfancybrownhats.com",
+                    title: "View Website",
+                  },
+                  {
+                    type: "postback",
+                    title: "Start Chatting",
+                    payload: "DEVELOPER_DEFINED_PAYLOAD",
+                  },
+                ],
+              },
+              {
+                title: "Welcome2!",
+                image_url: "https://petersfancybrownhats.com/company_image.png",
+                subtitle: "We have the right hat for everyone.",
+                default_action: {
+                  type: "web_url",
+                  url: "https://petersfancybrownhats.com/view?item=103",
+                  webview_height_ratio: "tall",
+                },
+                buttons: [
+                  {
+                    type: "web_url",
+                    url: "https://petersfancybrownhats.com",
+                    title: "View Website",
+                  },
+                  {
+                    type: "postback",
+                    title: "Start Chatting",
+                    payload: "DEVELOPER_DEFINED_PAYLOAD",
+                  },
+                ],
+              },
+              {
+                title: "Welcome3!",
+                image_url: "https://petersfancybrownhats.com/company_image.png",
+                subtitle: "We have the right hat for everyone.",
+                default_action: {
+                  type: "web_url",
+                  url: "https://petersfancybrownhats.com/view?item=103",
+                  webview_height_ratio: "tall",
+                },
+                buttons: [
+                  {
+                    type: "web_url",
+                    url: "https://petersfancybrownhats.com",
+                    title: "View Website",
+                  },
+                  {
+                    type: "postback",
+                    title: "Start Chatting",
+                    payload: "DEVELOPER_DEFINED_PAYLOAD",
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      };
+      await sendMessage(sender_psid, response);
+      resolve("done");
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
 module.exports = {
   sendMessage: sendMessage,
   sendMessageWelcomeNewUser: sendMessageWelcomeNewUser,
+  sendCategories: sendCategories,
 };
